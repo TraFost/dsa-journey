5c4dcef24e59b8848e334d7d98243b8c91592583
Rahman <rahwisdilfiqrak@gmail.com>
2024-07-17 14:15:03 +0700

    binary search

--- binary-search/code.js
+++ binary-search/code.js
@@ -1,24 +1,30 @@
-function binary_search(list, item) {
-	let low = 0;
-	let high = list.length - 1;
+function binarySearch(arr, target) {
+	// left and right pointers to keep track of the search range
+	let left = 0; // left pointer
+	let right = arr.length - 1; // right pointer
 
-	while (low <= high) {
-		const mid = Math.floor((low + high) / 2);
-		const guess = list[mid];
+	// while the left pointer is less than or equal to the right pointer, keep searching
+	while (left <= right) {
+		const val = Math.floor((left + right) / 2);
+		const guess = arr[val];
 
-		if (guess === item) {
-			return mid;
-		} else if (guess > item) {
-			high = mid - 1;
-		} else {
-			low = mid + 1;
+		// if the guess is equal to the target, we found the target
+		if (guess === target) {
+			return val;
+		} // if the guess is too high, search the left side
+		else if (guess > target) {
+			right = val - 1;
+		} // if the guess is too low, search the right side
+		else {
+			left = val + 1;
 		}
 	}
 
+	// if the target is not found, return null
 	return null;
 }
 
-const my_list = [1, 3, 5, 7, 9];
+const list = [1, 3, 5, 7, 9];
 
-console.log(binary_search(my_list, 3)); // 1
-console.log(binary_search(my_list, -1)); // null
+console.log(binarySearch(list, 3)); // 1
+console.log(binarySearch(list, -1)); // null
